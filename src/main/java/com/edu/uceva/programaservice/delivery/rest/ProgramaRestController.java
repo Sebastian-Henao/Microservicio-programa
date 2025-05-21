38715d018c4df1b9f9ead3975f0ff6acf5d63138
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2025-05-21 09:45:31 -0500

    Correcion

--- src/main/java/com/edu/uceva/programaservice/delivery/rest/ProgramaRestController.java
+++ src/main/java/com/edu/uceva/programaservice/delivery/rest/ProgramaRestController.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1/programa-service")
 public class ProgramaRestController {
